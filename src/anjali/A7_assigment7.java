d8bc71bd8dccd2a548a57c96a96eae1f4aeba608
Anjali Bidkar <bidkaranjalibapurao@gmail.com>
2021-12-19 21:21:38 +0530

    Anjli_A7

--- src/anjali/A7_assigment7.java
+++ src/anjali/A7_assigment7.java
@@ -3,8 +3,26 @@ package anjali;
 public class A7_assigment7 {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
-
+		A7_assigment7 a7=new A7_assigment7();
+		a7.printAtEven("Anjali");
+		a7.PrintVowel("anjaliu");
+		
+		
+	}
+	
+	void printAtEven(String name){
+		for(int index=0; index<name.length(); index++){
+			if(index%2==0)
+				System.out.println(name.charAt(index));
+		}
+	}
+	
+	void PrintVowel(String name){
+		for(int index=0; index<name.length(); index++){
+			if(name.charAt(index)=='a' || name.charAt(index)=='e' ||name.charAt(index)=='i'|| name.charAt(index)=='o' ||name.charAt(index)=='u')
+				System.out.println(name.charAt(index));
+		}
+		
 	}
 
 }
