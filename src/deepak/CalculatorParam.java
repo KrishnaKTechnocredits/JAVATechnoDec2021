6d0613a2e25c4ffd3f71d083910ceffb56015318
Deepak Kankhar <dgkankhar@gmail.com>
2021-12-19 11:56:13 +0530

    Deepak_Assignment_1-6

---
+++ src/deepak/CalculatorParam.java
@@ -0,0 +1,34 @@
+/*Assignment - 2: 14th Dec'2021
+Write a calculator program, all methods should take 2 parameters.*/
+package deepak;
+
+class CalculatorParam{
+	
+	void add(int num1, int num2){
+		int output = num1+num2;
+		System.out.println("Addition of numbers is: "+output);
+	}
+	
+	void substraction(int num1, int num2){
+		int output = num1-num2;
+		System.out.println("Substraction of numbers is: "+output);
+	}
+	
+	void multiplication(int num1, int num2){
+		int output = num1*num2;
+		System.out.println("Multiplication of numbers is: "+output);
+	}
+	
+	void division(int num1, int num2){
+		int output = num1/num2;
+		System.out.println("Division of numbers is: "+output);
+	}
+	
+	public static void main(String[] a){
+		CalculatorParam calculatorParam = new CalculatorParam();
+		calculatorParam.add(20,10);
+		calculatorParam.substraction(70,30);
+		calculatorParam.multiplication(35,7);
+		calculatorParam.division(44,11);
+	}
+}
