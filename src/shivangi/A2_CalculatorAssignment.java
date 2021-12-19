18a1c41d1bffea2b0f09f2a9d0f50624f41df90d
Shivangi Gupta <shgupta.shivangi@gmail.com>
2021-12-19 12:51:17 +0530

    RenamedAssignmentName

--- src/shivangi/A2_CalculatorAssignment.java
+++ src/shivangi/A2_CalculatorAssignment.java
@@ -1,6 +1,6 @@
 package shivangi;
 
-class CalculatorAssignment
+class A2_CalculatorAssignment
 {
 	void add(int num1, int num2){
 		int ans = num1+num2;
@@ -24,7 +24,7 @@ class CalculatorAssignment
 	
 	public static void main(String[] args)
 	{
-		CalculatorAssignment calculatorAssignment1 = new CalculatorAssignment();
+		A2_CalculatorAssignment calculatorAssignment1 = new A2_CalculatorAssignment();
 		calculatorAssignment1.add(8,5);
 		calculatorAssignment1.substract(15,6);
 		calculatorAssignment1.multi(5,5);
--- src/shivangi/A6_Example6.java
+++ src/shivangi/A6_Example6.java
@@ -1,6 +1,6 @@
 package shivangi;
 
-class Example6
+class A6_Example6
 {
 	void display(String name, int empid, String deptName){
 		System.out.println(empid);
@@ -8,7 +8,7 @@ class Example6
 	
 	public static void main(String[] args)
 	{
-		Example6 example6 = new Example6();
+		A6_Example6 example6 = new A6_Example6();
 		example6.display("Techno",123,"Credits");
 	}
 }
