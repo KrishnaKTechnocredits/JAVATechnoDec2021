1eb99d18f8fd4cbdcf34e8abeaea63509514f492
AshishBrahme <ashish.brahme@gmail.com>
2021-12-19 11:53:39 +0530

    Assignment 1 to 6

---
+++ src/ashish/AssignmentFive.java
@@ -0,0 +1,81 @@
+package ashish;
+
+class AssignmentFive{
+	
+	void printEvenNumbersForRange(int startNumber, int endNumber){
+		System.out.println(" Even numbers in range (" + startNumber + "," + endNumber + ") are below : ");
+		for(int i = startNumber; i <= endNumber; i++){
+			if(i % 2 == 0)	
+				System.out.println(" " + i);
+		}
+	}
+	
+	void printDivisibleByFive(int startNumber, int endNumber){
+		System.out.println(" Numbers divisible by 5 in range (" + startNumber + "," + endNumber + ") are below : ");
+		for(int i = startNumber; i <= endNumber; i++){
+			if(i % 5 == 0)	
+				System.out.println(" " + i);
+		}
+	}
+	
+	void printDivisibleByThreeAndFive(int startNumber, int endNumber){
+		System.out.println(" Numbers divisible by 3 and 5 in range (" + startNumber + "," + endNumber + ") are below : ");
+		for(int i = startNumber; i <= endNumber; i++){
+			if(i % 3 == 0 && i % 5 == 0)	
+				System.out.println(" " + i);
+		}
+	}
+	
+	void printDivisibleBySevenOrThirteen(int startNumber, int endNumber){
+		System.out.println(" Numbers divisible by 7 or 13 in range (" + startNumber + "," + endNumber + ") are below : ");
+		for(int i = startNumber; i <= endNumber; i++){
+			if(i % 7 == 0)
+				System.out.println(" " + i  + " is divisible by 7");
+			else if(i % 13 == 0)
+				System.out.println(" " + i  + " is divisible by 13");
+		}
+	}
+	
+	void sumOfNumbersForRange(int startNumber, int endNumber){
+		int sum = 0;
+		for(int i = startNumber; i <= endNumber; i++)
+			sum = sum + i;
+		System.out.println(" Sum of numbers in range (" + startNumber + "," + endNumber + ") is : " + sum);	
+	}
+	
+	void differenceBetweenSumOfOddEvenForRange(int startNumber, int endNumber){
+		int sumOfOdd = 0, sumOfEven = 0;
+		for(int i = startNumber; i <= endNumber; i++){
+			if(i % 2 == 0)
+				sumOfEven = sumOfEven + i;
+			else	
+				sumOfOdd = sumOfOdd + i;
+		}
+		System.out.println(" Difference between sum of even and odd numbers in range (" + startNumber + "," + endNumber + ") is : " + (sumOfOdd - sumOfEven));	
+	}
+	
+	void printOddNumbersInReverseForRange(int startNumber, int endNumber){
+		System.out.println(" Odd Numbers in reverse order in range (" + startNumber + "," + endNumber + ") is : ");	
+		for(int i = endNumber; i >= startNumber; i--){
+			if(i % 2 == 1)
+				System.out.println(" " + i);				
+		}		
+	}
+	
+	public static void main(String[] args){
+		AssignmentFive assignmentFive = new AssignmentFive();
+		assignmentFive.printEvenNumbersForRange(10,15);
+		System.out.println();				
+		assignmentFive.printDivisibleByFive(10,30);
+		System.out.println();
+		assignmentFive.printDivisibleByThreeAndFive(5,18);
+		System.out.println();
+		assignmentFive.printDivisibleBySevenOrThirteen(5,40);
+		System.out.println();
+		assignmentFive.sumOfNumbersForRange(1,5);
+		System.out.println();
+		assignmentFive.differenceBetweenSumOfOddEvenForRange(3,9);
+		System.out.println();
+		assignmentFive.printOddNumbersInReverseForRange(10,20);
+	}
+}
---
+++ src/ashish/StudentInfo.java
@@ -0,0 +1,35 @@
+package ashish;
+
+class StudentInfo{
+	
+	String name, middleName, surname, birthDate, address;
+	int rollNumber;
+	
+	void setData(){
+		name = "Ashish";
+		middleName = "Uday";
+		surname = "Brahme";
+		birthDate = "28th Oct 1992";
+		address = "G-809, Heaven Apartment, Baner, Pune.";
+		rollNumber = 28;
+	}
+	
+	void studentName(){
+		System.out.println(name);
+		System.out.println(middleName);
+		System.out.println(surname);
+	}
+	
+	void studentOtherDetails(){
+		System.out.println(birthDate);
+		System.out.println(address);
+		System.out.println(rollNumber);
+	}
+	
+	public static void main(String[] args){
+		StudentInfo studentInfo = new StudentInfo();
+		studentInfo.setData();
+		studentInfo.studentName();
+		studentInfo.studentOtherDetails();
+	}
+}
