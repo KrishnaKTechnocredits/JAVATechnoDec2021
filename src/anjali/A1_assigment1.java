31d0764d3171123aa5091401d6afd8b90354f84a
Anjali Bidkar <bidkaranjalibapurao@gmail.com>
2021-12-19 14:37:43 +0530

    RenameANjali

---
+++ src/anjali/A1_assigment1.java
@@ -0,0 +1,10 @@
+package anjali;
+
+public class A1_assigment1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+
+	}
+
+}
---
+++ src/anjali/A4_assigment4.java
@@ -0,0 +1,9 @@
+package anjali;
+
+public class A4_assigment4 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+
+}
