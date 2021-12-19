64592f371ff9866ed2c5fab293f71fd06eee553e
Monika Arkade <arkademonika89@gmail.com>
2021-12-19 10:39:26 +0530

    String Operations

---
+++ src/monikaArkade/StringOperations.java
@@ -0,0 +1,19 @@
+package monikaArkade;
+
+public class StringOperations {
+	
+	void findFreqOfChar(String input,char ch) {
+		int count = 0;
+		for(int index=0;index<input.length();index++) {
+			if(input.charAt(index)==ch) {
+				count++;
+			}
+		}
+		System.out.println("Count of "+ ch + " is : "+ count);
+	}
+	
+	public static void main(String[] args) {
+		StringOperations stringOperations = new StringOperations();
+		stringOperations.findFreqOfChar("monikaarkade", 'a');
+	}
+}
