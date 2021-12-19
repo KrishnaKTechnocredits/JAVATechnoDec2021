5476fb5f09e0c8e96e34ee20b0148e10a60283b7
Priya Sontakke <priya_sontakke@yahoo.com>
2021-12-19 15:56:08 +0530

    Priya_Assignment_6

---
+++ src/priya/A3_DaysOfWeek.java
@@ -0,0 +1,23 @@
+class DaysOfWeek{
+	
+		void dayDetermination(int dayIndex){
+			if(dayIndex >= 1 && dayIndex <= 5)
+				System.out.println("Day" +dayIndex+" is weekday.");
+			else if(dayIndex >= 6 && dayIndex <= 7)
+				System.out.println("Day " +dayIndex+" is weekend.");
+			else 
+				System.out.println(dayIndex+" is invalid index.");
+		}
+		
+		public static void main (String[] args){
+			DaysOfWeek dayIndex = new DaysOfWeek();
+			dayIndex.dayDetermination(1);
+			dayIndex.dayDetermination(2);
+			dayIndex.dayDetermination(3);
+			dayIndex.dayDetermination(4);
+			dayIndex.dayDetermination(5);
+			dayIndex.dayDetermination(6);
+			dayIndex.dayDetermination(7);
+			dayIndex.dayDetermination(8);
+		}				
+}
