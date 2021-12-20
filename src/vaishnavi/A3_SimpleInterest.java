4d0d9998cef132b0a898ea3d45ca7aa616bd8ea9
VaishnaviR2121 <vaishnaviramekar4@gmail.com>
2021-12-21 01:12:47 +0530

    Vaishnavi_Assign_1-6

---
+++ src/vaishnavi/A3_SimpleInterest.java
@@ -0,0 +1,19 @@
+package vaishnavi;
+class A3_SimpleInterest{
+	
+	void calculateInterest(int p, int t, float r){
+		double si =(p*t*r)/100;
+		System.out.println("Simple Interest is "+si);
+	}
+	
+	void convertTemp(float fahrenheit){
+		double celsius = (fahrenheit - 32) * 5 / 9;
+		System.out.println(" Temperature is "+celsius);
+	}
+	
+	public static void main(String[] args){
+		A3_SimpleInterest simpleinterest= new A3_SimpleInterest();
+		simpleinterest.calculateInterest(1500,10,20);
+		simpleinterest.convertTemp(56);
+	}
+}
