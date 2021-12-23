e7b6c956ab4fb5529ea766a90c7599caca562b51
Megha saha <megha.saha1306@yahoo.com>
2021-12-23 11:35:51 +0530

    Megha_Assignment_10

--- src/megha/Megha_Assignment_10.java
+++ src/megha/Megha_Assignment_10.java
@@ -2,7 +2,8 @@ package megha;
 
 public class Megha_Assignment_10 {
 
-	int count, sum, avg;
+	int count; 
+	double avg, sum;
 
 	void isPrime(int num) {
 		boolean flag = true;
@@ -16,25 +17,48 @@ public class Megha_Assignment_10 {
 			count++;
 			sum = sum + num;
 			avg = sum / count;
-			System.out.println(num + " is a Prime Number");
+			System.out.println(num + " is a Prime numbers");
 		}
+
 	}
 
 	void findPrimeInARange(int startNum, int endNum) {
-		sum = 0;
-		count = 0;
 		for (int num = startNum; num <= endNum; num++) {
 			isPrime(num);
 		}
+	}
+
+	void findCountOfPrimeInARange(int startNum, int endNum) {
+		count = 0;
+		findPrimeInARange(startNum, endNum);
 		System.out.println("Count of Prime numbers is : " + count);
+	}
+
+	void findSumOfPrimeInARange(int startNum, int endNum) {
+		sum = 0;
+		findPrimeInARange(startNum, endNum);
 		System.out.println("Sum of Prime numbers is : " + sum);
+	}
+
+	void findAverageOfPrimeInARange(int startNum, int endNum) {
+		avg = 0;
+		count = 0;
+		sum = 0;
+		findPrimeInARange(startNum, endNum);
 		System.out.println("Average of Prime numbers is : " + avg);
 	}
 
 	public static void main(String[] args) {
 		Megha_Assignment_10 megha_Assignment = new Megha_Assignment_10();
 		megha_Assignment.isPrime(17);
+		System.out.println();
 		megha_Assignment.findPrimeInARange(100, 120);
+		System.out.println();
+		megha_Assignment.findCountOfPrimeInARange(50, 100);
+		System.out.println();
+		megha_Assignment.findSumOfPrimeInARange(150, 180);
+		System.out.println();
+		megha_Assignment.findAverageOfPrimeInARange(20, 60);
 	}
 
 }
