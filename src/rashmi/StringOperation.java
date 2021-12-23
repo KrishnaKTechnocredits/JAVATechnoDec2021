5d07a3a5f7df17cc45d445aa087fe709d0aa529b
RashmiBante <rashmibante1995@gmail.com>
2021-12-23 23:21:26 +0530

    StringOperation

---
+++ src/rashmi/StringOperation.java
@@ -0,0 +1,12 @@
+package rashmi;
+
+public class StringOperation {
+	void calStrLength(String input) {
+		System.out.println(input + ":" + input.length());
+	}
+	
+	public static void main(String[] args) {
+		StringOperation stringOperation = new StringOperation();
+		stringOperation.calStrLength("technocredits");
+	}
+}
