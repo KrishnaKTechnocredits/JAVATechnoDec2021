6c5cd81afa711bf073f54b1a325ff3b91d572df4
Maulik Kanani <technocredits.oct2020@gmail.com>
2021-12-23 08:56:32 +0530

    Techno_Session_Primenum

---
+++ src/technocredits/PrimeNumber.java
@@ -0,0 +1,43 @@
+package technocredits;
+
+public class PrimeNumber {
+
+	void isPrimeNumber(int num){
+		boolean flag = true;
+		for(int index=2;index<=num/2;index++) {
+			if(num%index == 0) {
+				System.out.println(num + " is not a prime number");
+				flag = false;
+				break;
+			}
+		}
+		if(flag == true)
+			System.out.println(num + " is prime");
+	}
+	
+	void findPrimeNumberInRange(int fnum, int lnum) {
+		for(int startNum = fnum; startNum<=lnum; startNum++) {
+			isPrimeNumber(startNum);
+		}
+	}
+	
+	void totalPrimeNumbersInRange(int fnum, int lnum) {
+		for(int startNum = fnum; startNum<=lnum; startNum++) {
+			isPrimeNumber(startNum);
+		}
+		System.out.println(); // 26
+	}
+	
+	void displaySumOfPrimeNumbersInRange(int fnum, int lnum) {
+		for(int startNum = fnum; startNum<=lnum; startNum++) {
+			isPrimeNumber(startNum);
+		}
+		System.out.println(sum);
+	}
+	
+	public static void main(String[] args) {
+		PrimeNumber primeNumber = new PrimeNumber();
+		primeNumber.isPrimeNumber(44);
+		primeNumber.findPrimeNumberInRange(2,100);
+	}
+}
