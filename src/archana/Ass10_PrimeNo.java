181ec061c8caff1f862a19ff698e8f1df098082f
ArchanaJadhav <archananpawar11@gmail.com>
2021-12-24 22:26:30 +0530

    Archana_Ass10_PrimeNo

--- src/archana/Ass10_PrimeNo.java
+++ src/archana/Ass10_PrimeNo.java
@@ -26,7 +26,7 @@ class Ass10_PrimeNo
     }
      void countPrimeno( )
      {
-    	 System.out.println("Total prime no in a given range are :"+count);  
+    	 System.out.println("Total prime no in a given range are : "+count);  
 
      }
     void AllprimeNo(int startNo ,  int endNo  )
@@ -43,14 +43,15 @@ class Ass10_PrimeNo
     }
     public static void main(String[] args) {
 		Ass10_PrimeNo ass10=new Ass10_PrimeNo();
-		System.out.println("Prime no. List"); 
-		ass10.primeNo(17);
+		System.out.println("Prime no : "); 
+		ass10.primeNo(17);		
 		System.out.println("-----------------------------");
+		System.out.println("Prime no ni given range 100 to 200 are :"); 
 		ass10.AllprimeNo(100, 200);
 		System.out.println("-----------------------------");
 		ass10.countPrimeno();
 		System.out.println("-----------------------------");
-		System.out.println("Sum of prime no."+ sum);  
+		System.out.println("Sum of prime no : "+ sum);  
 		System.out.println("-----------------------------");
 		ass10.AvgprimeNo(sum,count);
 	}
