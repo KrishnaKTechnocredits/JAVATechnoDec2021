9200c109a2a55c403dfe18b58d1b96bd1a7ad00c
Swati P <swatisp2304@gmail.com>
2021-12-25 04:06:17 -0500

    Swati_Test1

--- src/swati/Test1/Test1_FindChar.java
+++ src/swati/Test1/Test1_FindChar.java
@@ -2,12 +2,18 @@ package swati.Test1;
 
 public class Test1_FindChar {
 
+	static int bal;
+	static void print() {
+		int val=bal;
+	}
+
 	int findChar(String name, char c) {
 		int count = 0;
 		char chr = 0;
 		for (int i = 0; i < name.length(); i++) {
 			if (name.charAt(i) == c) {
 				count++;
+				//System.out.println(count);
 
 			}
 
@@ -21,16 +27,20 @@ public class Test1_FindChar {
 		for (int i = 0; i < name.length(); i++) {
 			// System.out.println("find char at "+ name.charAt(i));
 			int count = findChar(name, name.charAt(i));
-			if (count >= 2) {
-				System.out.println(name.charAt(i) + " first repeated charcter");
+			if (count <= 1) {
+				System.out.println(name.charAt(i) + " first non repeated charcter");
 				break;
 			}
 
 		}
 	}
+	
+
 
 	public static void main(String args[]) {
+		
 		Test1_FindChar t1 = new Test1_FindChar();
-		t1.findString("tammy");
+		t1.findString("mississipi");
+		
 	}
 }
