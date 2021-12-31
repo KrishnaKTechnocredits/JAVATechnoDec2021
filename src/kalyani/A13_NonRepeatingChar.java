9be29355ea2d1c7253373f4043e25a2b23f5f976
Kalyani Jalekar <jalekarkalyani7@outlook.com>
2021-12-31 12:15:31 +0530

    Kalyani_Assignment_13-14

---
+++ src/kalyani/A13_NonRepeatingChar.java
@@ -0,0 +1,27 @@
+
+package kalyani;
+
+import java.util.Scanner;
+
+public class A13_NonRepeatingChar {
+
+	void findNonRepeatingChars(String str) {
+		System.out.println("Non Repeating Character : ");
+		for (int index = 0; index < str.length(); index++) {
+			char ch = str.charAt(index);
+			if (str.indexOf(ch) == str.lastIndexOf(ch))
+				System.out.print(ch + " ");
+		}
+	}
+
+	public static void main(String[] args) {
+		A13_NonRepeatingChar nonrepeatingchar = new A13_NonRepeatingChar();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the String : ");
+		String str = sc.next();
+		nonrepeatingchar.findNonRepeatingChars(str);
+		sc.close();
+	}
+
+}
+
