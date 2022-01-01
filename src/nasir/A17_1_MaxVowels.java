492a88093fe108f511a126cd0b9fe6e289bfdf28
Nasir-9 <md.nasirahmed@gmail.com>
2022-01-01 18:06:54 +0530

    Nasir_Assignment_17

---
+++ src/nasir/A17_1_MaxVowels.java
@@ -0,0 +1,29 @@
+package nasir;
+class A17_1_MaxVowels {
+	void findMaximumVowles(String[] input){
+		String nameOfMaximumVowel =" ";
+		int cnt=0;
+		for(int index=0; index<input.length;index++){
+			String temp =input[index];
+			int count=0;			
+			for(int innerIndex=0; innerIndex<temp.length();innerIndex++){
+			    char ch=temp.charAt(innerIndex);
+				if(ch=='a'|| ch=='e'|| ch=='i'||ch=='o'||ch=='u')
+					count++;
+			}
+			System.out.println(temp+" has total count of Vowles "+count);
+				if(count>cnt){
+				cnt=count;
+				nameOfMaximumVowel=temp;
+		}
+		}
+		System.out.println(".............................");
+		System.out.println("Name of Maximum count of vowel is "+nameOfMaximumVowel);
+	}
+	
+	public static void main(String[]args){
+		A17_1_MaxVowels maxvowels = new A17_1_MaxVowels();
+		String[] name = {"hi","priyanka", "sandip", "aakanksha", "pallavi", "apurva"};
+		maxvowels.findMaximumVowles(name);
+	}
+}
