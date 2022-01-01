a5fb1c7a0fe0d5bfbb5a827c0e43511ebfacb0cd
Sandip Patil <sandipvitthalpatil@gmail.com>
2022-01-01 22:04:28 +0530

    Sandip_Assignment_17

---
+++ src/sandip/Assignment_17/Triplet.java
@@ -0,0 +1,19 @@
+package sandip.Assignment_17;
+
+public class Triplet {
+
+	void triplet(int[] num) {
+		for (int index = 0; index < num.length - 2; index++) {
+			if (num[index + 1] == num[index] + 1 && num[index + 2] == num[index] + 2) {
+				System.out.println(num[index] + "--" + num[index + 1] + "--" + num[index + 2]);
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Triplet method = new Triplet();
+		int[] n1 = { 10, 13, 14, 15, 16, 19, 20, 22, 23, 24 };
+		method.triplet(n1);
+	}
+}
