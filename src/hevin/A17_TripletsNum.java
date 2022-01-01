de7f89543fe0b6620b6daf82a76858b7e8f6b7be
Hevin Patel <hevin1016@gmail.com>
2022-01-01 21:11:19 +0530

    Hevin_Assignment_17_Array

---
+++ src/hevin/A17_TripletsNum.java
@@ -0,0 +1,18 @@
+package hevin;
+
+class A17_TripletsNum {
+
+	void findConsecutiveNumbers(int[] num){
+		for(int index=0;index<num.length-2;index++){
+			if(num[index]+1==num[index+1] && num[index]+2==num[index+2])
+				System.out.println("--> " + num[index]+" "+(num[index]+1)+" "+(num[index]+2));
+		}
+	}
+
+	public static void main(String[] args){	
+	
+		A17_TripletsNum triplets = new A17_TripletsNum();
+		int[] arr = {10,13,14,15,16,19,20,22,23,24};
+		triplets.findConsecutiveNumbers(arr);
+	}
+} 
