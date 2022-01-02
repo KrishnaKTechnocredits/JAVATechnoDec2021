8159f40727da096c2a717fbbc319607f71f581ae
pratiksha jamdade <pratikshajnec@gmail.com>
2022-01-02 19:16:39 +0530

    A13_DuplicateChar

--- src/pratiksha/A13_DuplicateChar.java
+++ src/pratiksha/A13_DuplicateChar.java
@@ -1,9 +1,28 @@
 package pratiksha;
 
+import java.util.Scanner;
+
 public class A13_DuplicateChar {
 
+	void duplicateCharacter(String name) {
+    	char ch =' ';
+    	for(int index=0; index<name.length(); index++) {
+    		ch=name.charAt(index);
+    		if(name.indexOf(ch)!=name.lastIndexOf(ch)) {
+    			System.out.println("duplicate characters are :"+ch);
+    		}
+    	}
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+	A13_DuplicateChar duplicatechar = new A13_DuplicateChar();
+	System.out.println("Enter Word: ");
+	String name = sc.nextLine();
+	duplicatechar.duplicateCharacter(name);
+	
 
 	}
 
