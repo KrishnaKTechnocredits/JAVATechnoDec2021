f9b3b2644cca56365380c5f920959a8a8024dd3b
Priyanka Kamble <priyankakamble1249@gmail>
2022-01-02 23:16:44 +0530

    Priyanka Kamble - Assignment 18 - Inheritance

---
+++ src/priyankaKamble/assignment_18/case_2/Case2_ClassA.java
@@ -0,0 +1,16 @@
+package priyankaKamble.assignment_18.case_2;
+/* Case 2 Inheritance*/
+
+public class Case2_ClassA {
+	int x = 10;
+	int y = 20;
+
+	void m1() {
+		System.out.println("Case 2 - Class A- in m1 Method ");
+	}
+
+	void m2() {
+		System.out.println("Case 2 - Class A- in m2 Method ");
+	}
+	// There is no Main method in Class A
+}
---
+++ src/priyankaKamble/assignment_18/case_4/Case4_ClassA.java
@@ -0,0 +1,17 @@
+package priyankaKamble.assignment_18.case_4;
+/* Case 4 -> Invalid Case*/
+
+public class Case4_ClassA {
+	int x = 10;
+	int y = 20;
+
+	void m1() {
+		System.out.println("Case 4 - Class A- in m1 Method ");
+	}
+
+	void m2() {
+		System.out.println("Case 4 - Class A- in m2 Method ");
+	}
+	// There is no Main method in Class A
+	// Case4_ClassA case4A = new Case4_ClassA();
+}
