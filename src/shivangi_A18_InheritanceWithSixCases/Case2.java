91ba4d47e8e193107d7dccd2d75ae9d54a951f67
Shivangi Gupta <shgupta.shivangi@gmail.com>
2022-01-03 22:42:59 +0530

    Shivangi_Assignment18_InheritanceWithSixCases

---
+++ src/shivangi_A18_InheritanceWithSixCases/Case2.java
@@ -0,0 +1,17 @@
+package shivangi_A18_InheritanceWithSixCases;
+
+public class Case2 {
+	
+	public static void main(String[] args) {
+
+		Car car = new Car();
+		System.out.println(car.name);
+		System.out.println(car.carColour);
+
+		car.carDetails(5440);
+		car.vehicleDetails(200);
+		car.display();
+
+	}
+	
+}
