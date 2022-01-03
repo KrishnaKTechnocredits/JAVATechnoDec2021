aaeddf6f61c89a647d57031558abf4903581e3d3
AnvitPTechnocredits <96311751+AnvitPTechnocredits@users.noreply.github.com>
2022-01-03 22:36:00 +0530

    Update Case1.java

--- src/anvit/Inheritance_Cases/Case1.java
+++ src/anvit/Inheritance_Cases/Case1.java
@@ -6,10 +6,9 @@ public class Case1 {
 		A18_Test1 c1 = new A18_Test1();
 		System.out.println(c1.x);
 		System.out.println(c1.y);
-		//System.out.println(test1.z); CE Because z does not belong in class A18_Inheritance1
+		//System.out.println(c1.z); CE Because z does not belong in class A18_Test1
 
 		c1.m1();
 		c1.m2();
-		//test1.m3(); // CE Because m3() does not belong in class A18_Test1
 	}
 }
