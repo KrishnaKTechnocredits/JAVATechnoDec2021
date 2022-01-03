2fbc29e7f2cc2d6a695ca759ae682fe981959155
Jayashri Kotwal <kotwaljayashri97@gmail.com>
2022-01-04 00:08:01 +0530

    Assignment_18

---
+++ src/jayashri/A18_ClassB.java
@@ -0,0 +1,13 @@
+package jayashri;
+
+public class A18_ClassB extends A18_ClassA {
+	int y = 50;
+	int z = 70;
+
+	void m2() {
+		System.out.println("B m2");
+	}
+	void m3() {
+		System.out.println("B m3");
+	}
+}
