c757118db9c715da91a3d40605228bcd2dd87a98
Vishakha Rokade <vishakharokade18@gmail.com>
2022-01-03 14:20:54 +0530

    A18_Inheritance.java

---
+++ src/vishakha/A18_InheritanceCase1.java
@@ -0,0 +1,8 @@
+package vishakha;
+
+public class A18_InheritanceCase1 {
+	
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/vishakha/A18_InheritanceCase2.java
@@ -0,0 +1,13 @@
+package vishakha;
+
+public class A18_InheritanceCase2 {
+	public static void main(String[] args) {
+		A18_InheritanceB b= new A18_InheritanceB();
+		System.out.println(b.x);
+		System.out.println(b.z);
+		
+		b.m1();
+		b.m2();
+		b.m3();
+		}
+}
