b9a00e505f9fd9a50d80dff07029c36231e637a7
VaishnaviR2121 <vaishnaviramekar4@gmail.com>
2022-01-03 18:27:02 +0530

    Vaishnavi_Assignment_18

---
+++ src/vaishnavi/A18_Case4.java
@@ -0,0 +1,17 @@
+package vaishnavi;
+
+public class A18_Case4 {
+
+	public static void main(String[] args) {
+
+		/*	A18_Test2 test2 = new A18_Test1(); Compile time error,as we can not convert parent class(A18_Test1)into child class(A18_Test2). 
+			
+			System.out.println(test2.x);
+			System.out.println(test2.y);
+			System.out.println(test2.z);
+			
+			test2.m1();
+			test2.m2();
+			test2.m3(); */
+		}
+}
---
+++ src/vaishnavi/A18_Test1.java
@@ -0,0 +1,15 @@
+package vaishnavi;
+
+public class A18_Test1 {
+	
+	int x=10;
+	int y=12;
+
+	void m1() {
+		System.out.println("Test1 m1");
+	}
+
+	void m2() {
+		System.out.println("Test1 m2");
+	}
+}
