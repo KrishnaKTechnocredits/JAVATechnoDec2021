95e0c14aef5cdf77d18d1723c9fd5e5b3317519f
Swati P <swatisp2304@gmail.com>
2022-01-03 18:56:50 -0500

    Swati_Assignment18

---
+++ src/swati/Swati_Assignment18/Case2.java
@@ -0,0 +1,16 @@
+package swati.Swati_Assignment18;
+
+public class Case2 extends Case1 {
+
+	int y = 50;
+	int z = 70;
+
+	void m2() {
+		System.out.println("B M2");
+	}
+
+	void m3() {
+		System.out.println("B M3");
+	}
+
+}
