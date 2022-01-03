fa6906403d205918c56360ea212aee3d5beacc03
Monika Arkade <arkademonika89@gmail.com>
2022-01-03 19:45:20 +0530

    MonikaArkade_Assignment18

---
+++ src/monikaArkade/A18_College.java
@@ -0,0 +1,12 @@
+package monikaArkade;
+
+public class A18_College extends A18_Student {
+
+	void getData() {
+		name = "Monika";
+		rollno = 2015;
+		System.out.println(name);
+		System.out.println(rollno);
+	}
+
+}
---
+++ src/monikaArkade/A18_Student.java
@@ -0,0 +1,12 @@
+package monikaArkade;
+
+public class A18_Student {
+	String name = "Kajal";
+	int rollno = 2013;
+
+	void getData() {
+		System.out.println(name);
+		System.out.println(rollno);
+	}
+
+}
