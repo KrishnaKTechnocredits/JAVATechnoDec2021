45f818db3925467415ce4b9619878678817a2cda
PoojaRajguru <poojarajguru3@gmail.com>
2022-01-03 18:07:20 -0700

    PoojaR_assignment_18_Inheritance

---
+++ src/poojaRajguru/A18_Case4.java
@@ -0,0 +1,21 @@
+package poojaRajguru;
+
+public class A18_Case4 {
+
+/*public static void main(String[] args) {
+		
+		A18_InheritanceClassB b1 = new A18_InheritanceClassA();
+		
+		System.out.println(b1.x);
+		System.out.println(b1.y);
+		System.out.println(b1.z); // B class variable
+				
+		b1.m1();
+		b1.m2();
+		b1.m3();
+	}
+}
+/*we can not create class A object i.e 
+ * parent class object with refference of child class*/
+}
+ 
