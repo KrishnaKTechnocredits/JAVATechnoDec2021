39faf8734a7da095c16ae4658b402859b57482ba
Yogesh Nagar <yogesh61189@gmail.com>
2022-01-05 10:30:12 +0530

    YogeshNagar_Assignment18_CharMethods

--- src/yogeshNagar/A18_CharacterClassLogic.java
+++ src/yogeshNagar/A18_CharacterClassLogic.java
@@ -2,8 +2,7 @@ package yogeshNagar;
 
 public class A18_CharacterClassLogic {
 	
-	void getCharctorVariation() {
-		String name = "TE1ch2noC2red9iTs";
+	void getCharctorVariation(String name) {
 		System.out.println("Total Char count in word is " + name.length());
 		int charCount=0;
 		int digCount=0;
@@ -24,14 +23,14 @@ public class A18_CharacterClassLogic {
 				lowerCaseCount++;
 		}
 		System.out.println("Letter Count is "+ charCount);
-		System.out.println("Uppser Case Letter Count is "+ upperCaseCount);
+		System.out.println("Upper Case Letter Count is "+ upperCaseCount);
 		System.out.println("Lower Case Letter Count is "+ lowerCaseCount);
 		System.out.println("Digit Count is "+ digCount);
 	}
 	
 	public static void main(String[] args) {
 		A18_CharacterClassLogic charLog = new A18_CharacterClassLogic();
-		charLog.getCharctorVariation();
+		charLog.getCharctorVariation("TE1ch2noC2red9iTs");
 	}
 
 }
