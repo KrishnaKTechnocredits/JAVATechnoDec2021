60815f536c813d8f2e9b40a3c3aed6248dea5c21
Debasmita Mishra <debasmitamishra6@gmail.com>
2022-01-06 21:46:52 +0530

    Debasmita_A18Inheritance

---
+++ src/debasmita/A18_InheritanceBMW.java
@@ -0,0 +1,13 @@
+package debasmita;
+
+public class A18_InheritanceBMW extends A18_InheritanceCar {
+       int registrationNo=5163;
+       int efficiency=18;
+      
+       void start() {
+     	  System.out.println("BMW-----Start");
+       }
+       void refuel() {
+    	   System.out.println("BMW Refuel Method");
+       }
+}
