994ebfe8dc847e21b8b5d7b2229f9a74fa80caba
Swati P <swatisp2304@gmail.com>
2022-01-09 02:52:40 -0500

    Changes_to_Swati_Assignment21

--- src/swati/A21_StringCount.java
+++ src/swati/A21_StringCount.java
@@ -1,25 +1,25 @@
 package swati;
 
 public class A21_StringCount {
-	
+
 	String inputCount(String name) {
-		String temp="";
-		for(int i=0;i<name.length();i++) {
-			char c= name.charAt(i);
-			if(Character.isDigit(c)) {
-				temp+=c;
+		String temp = "";
+		for (int i = 0; i < name.length(); i++) {
+			char c = name.charAt(i);
+			if (Character.isDigit(c)) {
+				temp += c;
 			}
-			
+
 		}
-		
-		return temp;	
+
+		return temp;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		A21_StringCount a21=new A21_StringCount();
-		String temp=a21.inputCount("T1e3h45n9O");
+
+		A21_StringCount a21 = new A21_StringCount();
+		String temp = a21.inputCount("T1e3h45n9O");
 		System.out.println(temp);
 
 	}
