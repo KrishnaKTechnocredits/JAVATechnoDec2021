0bbcf31ccb374f9a9a5af412f0f022eb654a4363
Kalyani Jalekar <jalekarkalyani7@outlook.com>
2022-01-13 18:40:07 +0530

    Kalyani_Test

---
+++ src/kalyani/Test.java
@@ -0,0 +1,18 @@
+package kalyani;
+
+public class Test {
+	
+	void display() {
+		
+		System.out.println("In Test");
+	}
+	
+	public static void main (String args[]) {
+	
+	Test t = new Test();
+	t.display();
+	
+	
+
+}
+}
