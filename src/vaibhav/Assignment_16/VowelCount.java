17342617f0e9504eea22d3e6ea78b48906fce80b
VaibhavTati <vaibhavtati@gmail.com>
2022-01-16 20:59:29 +0530

    Vaibhav_Assignment_16

---
+++ src/vaibhav/Assignment_16/VowelCount.java
@@ -0,0 +1,47 @@
+/*
+Assignment - 16 : 31st Dec'2021 
+
+print count of vowels in each input from String array and print total vowels.
+input : String[] arr = {"priyanka", "swati", "acharya", "hevin" }
+output : priyanka -> 3
+              swati -> 2
+              acharya -> 3
+              hevin -> 2
+total vowels -> 10
+
+*/
+package vaibhav.Assignment_16;
+
+public class VowelCount {
+
+	int totalVowelCount;
+
+	int getVowelCount(String str) {
+		char ch = ' ';
+		int count = 0;
+		for (int index = 0; index < str.length(); index++) {
+			ch = str.charAt(index);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+				totalVowelCount++;
+				count++;
+			}
+		}
+		return count;
+	}
+
+	void printVowelCount(String[] inputArr) {
+		String temp = "";
+		System.out.println("Vowel count for each word is as below : ");
+		for (int index = 0; index < inputArr.length; index++) {
+			temp = inputArr[index];
+			System.out.println(temp + "	:" + getVowelCount(temp));
+		}
+		System.out.println("\nTotal vowel count is : " + totalVowelCount);
+	}
+
+	public static void main(String[] args) {
+		VowelCount vowelCount = new VowelCount();
+		String[] arr = { "priyanka", "swati", "acharya", "hevin" };
+		vowelCount.printVowelCount(arr);
+	}
+}
