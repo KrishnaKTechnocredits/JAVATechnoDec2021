8080dff6b59ba8be28ec0d6a9d1f716f1c22dfc9
Saurav Tondare <tondaresaurav2014@gmail.com>
2022-01-19 00:16:51 +0530

    Saurav_Assignment_24

---
+++ src/saurav/A10_PrimeNum.java
@@ -0,0 +1,59 @@
+package saurav;
+
+public class A10_PrimeNum {
+
+	int sum=0;
+	int count=0;
+		void isPrimeNum(int num) {	
+
+			boolean flag= true;
+			for(int index=2; index<=num/2; index++) {
+				if(num%index == 0) {
+				//System.out.println(num+" It is not a prime num");
+					flag=false;
+					break;
+				}	
+			}
+				if(flag==true) {
+				count++;
+				sum=sum+num;
+				System.out.println(num+ " is a prime num");
+				}
+		}
+
+		void totalPrimeNumInRange(int fNum, int lNum) {
+			for( int startNum=fNum; startNum<=lNum; startNum++) {
+				isPrimeNum(startNum);
+			}
+		} 
+
+		void countOfPrimeNum(int fNum,int lNum ) {	
+			for( int startNum=fNum; startNum<=lNum; startNum++) {
+				 isPrimeNum(startNum);
+				 break;
+			}
+			System.out.println("Count of all prime num = "+count);
+		}	
+
+		void sumOfAllPrimeNum() {
+			System.out.println("Sum of all prime numbers = "+sum);
+		}	
+
+		void averageOfAllPrimeNum() {
+			int average = sum/count; 
+			System.out.println("averge of all prime numbers = "+average);
+		}
+
+		public static void main(String[] a) {
+			A10_PrimeNum prime = new A10_PrimeNum();
+			prime.isPrimeNum(41);
+			System.out.println();
+			prime.totalPrimeNumInRange(10,20);
+			System.out.println();
+			prime.countOfPrimeNum(10,20 );
+			System.out.println();
+			prime.sumOfAllPrimeNum();
+			prime.averageOfAllPrimeNum();
+		}
+	}
+	
