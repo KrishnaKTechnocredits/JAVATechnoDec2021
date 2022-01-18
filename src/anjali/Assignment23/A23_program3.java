454541c682d2d8eeab3edff5355439a2f153b6f6
Anjali Bidkar <bidkaranjalibapurao@gmail.com>
2022-01-18 23:02:02 +0530

    Anjali_Assignment23

---
+++ src/anjali/Assignment23/A23_program3.java
@@ -0,0 +1,37 @@
+/*Program 3 : Return a string by placing all small characters first, 
+ * followed by digits, capital letters, special characters.
+input : te4c&Hno7CreD-8i*ts
+output : tecnoreits478HCD&-*
+ */
+package anjali.Assignment23;
+
+public class A23_program3 {
+
+	static String collectAllChar(String name) {
+		String total = "";
+		String lowercase = "";
+		String uppercase = "";
+		String digit = "";
+		String SpecialChar = "";
+		for (int index = 0; index < name.length(); index++) {
+			if (Character.isLetter(name.charAt(index))) {
+				if (Character.isLowerCase(name.charAt(index)))
+					lowercase = lowercase + name.charAt(index);
+				else
+					uppercase = uppercase + name.charAt(index);
+			} else if (Character.isDigit(name.charAt(index)))
+				digit = digit + name.charAt(index);
+			else
+				SpecialChar = SpecialChar + name.charAt(index);
+		}
+		total = lowercase + uppercase + digit + SpecialChar;
+		return total;
+
+	}
+
+	public static void main(String[] args) {
+		String output = collectAllChar("te4c&Hno7CreD-8i*ts");
+		System.out.println(output);
+	}
+
+}
