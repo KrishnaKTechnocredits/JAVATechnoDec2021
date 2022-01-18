f5de3fcf7d7d1fef4d96f0bef31049e685ac4f87
Nishika shah <nishikashah121@gmail.com>
2022-01-18 23:13:00 +0530

    Test_1_Nishika

--- src/nishika/Test_1_Nishika.java
+++ src/nishika/Test_1_Nishika.java
@@ -1,6 +1,7 @@
 package nishika;
 
 public class Test_1_Nishika {
+	
 	void finduniquechar(int[] arr) {
 		for (int index = 0; index < arr.length; index++) {
 			boolean flag = true;
@@ -17,9 +18,25 @@ public class Test_1_Nishika {
 		}
 	}
 	
+	void findPairs(int[] array) {
+		int sum = 6;
+		
+		for(int i=0 ; i < array.length ; i++) {
+			
+			for(int j=i+1 ; j <array.length ; j++) {
+				
+				if(array[i] + array[j] == sum) {
+					System.out.println(array[i]+"..."+array[j]);
+				}
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		Test_1_Nishika test = new Test_1_Nishika();
 		int[] arr = {5,2,2,2,1};
 		test.finduniquechar(arr);
+		int[] array = {5,1,2,7,6,3,4,9};
+		test.findPairs(array);
 	}
 }
