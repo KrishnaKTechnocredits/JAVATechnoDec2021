70224c8be6bb88800eabfadcbf279fdc4974db02
Prajakta Shinde <shindeprajakta3198@gmail.com>
2022-01-22 22:54:06 +0530

    Prajakta_Assignment_24

---
+++ src/prajakta/Assignment24_SetFirstCharCaps.java
@@ -0,0 +1,31 @@
+package prajakta;
+
+public class Assignment24_SetFirstCharCaps {
+
+	String reverseDataWithFirstCaps(String str) {
+		String revString = "";
+		for (int index = 0; index < str.length(); index++) {
+			char ch = str.charAt(index);
+			if (index == 0)
+				ch = Character.toUpperCase(ch);
+			revString += ch;
+		}
+		return revString;
+
+	}
+
+	void spiltWord(String str) {
+		String[] data = str.split(" ");
+		String col = "";
+		for (int index = 0; index < data.length; index++) {
+			col = col + " " + reverseDataWithFirstCaps(data[index]);
+		}
+		System.out.println(col.trim());
+	}
+
+	public static void main(String[] args) {
+		Assignment24_SetFirstCharCaps ass24 = new Assignment24_SetFirstCharCaps();
+		ass24.spiltWord("anvit harsh dipali");
+	}
+
+}
---
+++ src/prajakta/Assignment24_WordLength.java
@@ -0,0 +1,19 @@
+package prajakta;
+
+public class Assignment24_WordLength {
+	void separateWordsLength(String input) {
+		System.out.println(input + "--> " + input.length());
+	}
+
+	void spiltWordsInString(String input) {
+		String[] arr = input.split(" ");
+		for (int index = 0; index < arr.length; index++) {
+			separateWordsLength(arr[index]);
+		}
+	}
+
+	public static void main(String[] args) {
+		Assignment24_WordLength ass24 = new Assignment24_WordLength();
+		ass24.spiltWordsInString("Hi Hello");
+	}
+}
