30ca557cd677485d9244908ab60e57811354f455
Swati P <swatisp2304@gmail.com>
2022-01-24 14:44:27 -0500

    Swati_Test3

---
+++ src/swati/A27_Mobile.java
@@ -0,0 +1,28 @@
+package swati;
+
+import java.util.Scanner;
+
+public class A27_Mobile {
+	
+	void checkValid(String num) {
+		
+		if(num.startsWith("+91 ") && num.length()==14) {
+			System.out.println(num);
+		}
+		else {
+			System.out.println("invalid Number");
+		}
+	}
+
+	public static void main(String[] args) {
+		A27_Mobile a27=new A27_Mobile();
+		 Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter Mobile:");
+	    String mobile = sc.nextLine();
+	    a27.checkValid(mobile);
+	    
+	   
+	}
+
+}
+
