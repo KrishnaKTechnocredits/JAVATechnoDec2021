71f2feee75f9fead04a4de6b8e09d1292806116f
Shamli Gadekar <shamligadekar04@gmail.com>
2022-01-26 13:41:17 +0530

    Assignment 25

---
+++ src/shamli/Assignment25FindMissingNumberFromArray.java
@@ -0,0 +1,23 @@
+package shamli;
+
+/*
+Program 1 : Find the single missing number from the array.*
+int num[] = { 1, 2, 8, 10, 9, 5, 7, 6, 4 };
+output: 3
+
+ */
+
+public class Assignment25FindMissingNumberFromArray {
+
+	public static void main(String[] args) {
+		int num[]= { 1, 2, 8, 10, 9, 5, 7, 6, 4 };
+		int n= num.length+1;
+		int expectedSum=(n*(n+1)/2);
+		
+		for(int i=0;i<num.length;i++)
+		{
+			expectedSum=expectedSum-num[i];
+		}
+		System.out.println("The missing number from the array is : "+ expectedSum);
+	}
+}
