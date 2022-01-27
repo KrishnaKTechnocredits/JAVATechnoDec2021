1a2dbe5d00d405bb4a6951adfe9862e9fad469cf
Yogesh Nagar <yogesh61189@gmail.com>
2022-01-27 18:19:11 +0530

    YogeshNagar_Exam4

--- src/yogeshNagar/Exam4_SquareOfArray.java
+++ src/yogeshNagar/Exam4_SquareOfArray.java
@@ -2,19 +2,25 @@ package yogeshNagar;
 
 public class Exam4_SquareOfArray {
 
-	void getNextNumberSquareInArray(int[] arr) {
+	boolean getNextNumberSquareInArray(int[] arr) {
+		boolean flag=false;
 		for (int index = 0; index < arr.length - 1; index++) {
 			int sqrInt = arr[index] * arr[index];
-			if (sqrInt == arr[index + 1]) {
-				System.out.println(arr[index] + " Next number in array is square " + arr[index + 1]);
-			}
+			if (sqrInt == arr[index + 1])
+				flag= true;
+			else
+				flag=false;
 		}
+		if(flag==true)
+			return true;
+		else
+			return false;
 	}
 
 	public static void main(String[] args) {
 		Exam4_SquareOfArray sqrNumArr = new Exam4_SquareOfArray();
 		int[] arr = { 2, 4, 16, 256 };
-		sqrNumArr.getNextNumberSquareInArray(arr);
+		System.out.println(sqrNumArr.getNextNumberSquareInArray(arr));
 	}
 
 }
