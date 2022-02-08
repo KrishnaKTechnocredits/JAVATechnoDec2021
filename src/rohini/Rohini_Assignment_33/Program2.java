73f7141b8408ef8ade75943b02ee11726970b650
Rohini Bansode <bansoderohini06@gmail.com>
2022-02-08 20:37:23 +0530

    Updating Ex 5,6,7 in prog 2 of assignment 33

--- src/rohini/Rohini_Assignment_33/Program2.java
+++ src/rohini/Rohini_Assignment_33/Program2.java
@@ -18,7 +18,13 @@ h -> 1
 
 Program 6: 
 Find all character frequency from each name given in String array.
-Input : Array -> {"raj", "aarya", "aavruti", "shruti","raj", "aarya"}*/
+Input : Array -> {"raj", "aarya", "aavruti", "shruti","raj", "aarya"}
+
+
+Program 7: 
+Return the frequency of each numbers from given array.
+input: [1,8,6,3,4,5,2,3,6,9,8,4]
+ */
 
 public class Program2 {
 
@@ -35,38 +41,57 @@ public class Program2 {
 				linkedhmap.put(ch, 1);
 			}
 
-		}System.out.println(linkedhmap);
+		}System.out.println("\n"+linkedhmap);
 	}
 
 
 
 	public void findnameFreqfromstr(String[] input) {
-		for(int i=0;i<input.length;i++){
-			String[] str=input[i].split(", ");
-			Map<String,Integer> treemap = new TreeMap<>();
-			int count=0; 
-			for(int index=0;index<str.length;index++) {
-				String s=str[index];
-				if(treemap.containsKey(s)) {
-					count=treemap.get(s);
-					treemap.put(s, count+1);
-				}
-				else {
-					treemap.put(s, 1);
-				}
+
+		Map<String,Integer> treemap = new TreeMap<>();
+
+		for(int index=0;index<input.length;index++) {
+			String s=input[index];
+			if(treemap.containsKey(s)) {
+				int count=treemap.get(s);
+				treemap.put(s, count+1);
 			}
-			System.out.print(treemap);		
-
-
+			else {
+				treemap.put(s, 1);
+			}
 		}
+		System.out.println("\n"+treemap);		
+
+
+	}
+
+	public void findFrequencyOfNumber(int[] num) {
+		Map<Integer,Integer> treemap=new TreeMap<Integer,Integer>();
+
+		for(int index=0;index<num.length;index++) {
+			int n=num[index];
+			if(treemap.containsKey(n)) {
+				int count=treemap.get(n);
+				treemap.put(n, count+1);
+			}
+			else {
+				treemap.put(n, 1);
+			}
+
+
+		}System.out.println("\n"+treemap);
 	}
 
 
 	public static void main(String[] args) {
 		Program2 prg2=new Program2();
 		prg2.findCharFreq("RohiniMali");
+
 		String[] arr= {"raj", "aarya", "aavruti", "shruti","raj", "aarya"};
 		prg2.findnameFreqfromstr(arr);
+
+		int[] num= {1,8,6,3,4,5,2,3,6,9,8,4};
+		prg2.findFrequencyOfNumber(num);
 
 	}
 
