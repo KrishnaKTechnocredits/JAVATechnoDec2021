987b28467ae6c45b003e7a93e3e4ba69cf41e4b3
Prajakta Shinde <shindeprajakta3198@gmail.com>
2022-02-10 09:43:44 +0530

    Prajakta_Test_6

---
+++ src/prajakta/MaxLength.java
@@ -0,0 +1,19 @@
+package prajakta;
+
+public class MaxLength {
+
+	public static void main(String[] args) {
+
+		String name = "good morning technocredits hi hello";
+		String[] arr = name.split(" ");
+		String max = "";
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i].length() < arr[i + 1].length() && max.length() < arr[i + 1].length()) {
+				max = arr[i + 1];
+			}
+		}
+		System.out.println(max);
+
+	}
+
+}
